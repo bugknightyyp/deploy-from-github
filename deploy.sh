3a53ce119d437959887a1e95a683dde51ec706d9
bugknightyyp <bugknightyyp@gmail.com>
2015-07-20 13:39:17 +0800

    improvement deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,9 @@ BLOG_PATH='/home/yyp/blog'
 echo "Start deployment"
 cd $BLOG_PATH
 git pull
-pm2 flush
-pm2 restart all
+sudo pm2 flush
+sudo pm2 restart all
+sleep 10s
+wget --post-data='' http://blog.yyper.com/manager/flush/gallery
+wget --post-data='' http://blog.yyper.com/manager/flush/life
+wget --post-data='' http://blog.yyper.com/manager/flush/it
