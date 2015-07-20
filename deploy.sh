7ac5d309c4aca28835c2fab47cd15f3edaf0ae2e
bugknightyyp <bugknightyyp@gmail.com>
2015-07-20 14:03:09 +0800

    add logs function

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,17 @@ BLOG_PATH='/home/yyp/blog'
 
 echo "Start deployment"
 cd $BLOG_PATH
+echo "excute pwd"
+pwd
+echo "pull"
 git pull
+echo "flush"
 sudo pm2 flush
-sudo pm2 restart all
+echo "restart"
+sudo pm2 restart blog
+echo "sleep 10s"
 sleep 10s
+echo "wget"
 wget --post-data='' http://blog.yyper.com/manager/flush/gallery
 wget --post-data='' http://blog.yyper.com/manager/flush/life
 wget --post-data='' http://blog.yyper.com/manager/flush/it
